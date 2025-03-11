d31237f54b98eae78bcab19c33888c80e66fa358
kellyserafico <seraficok@gmail.com>
2025-03-10 23:08:44 -0700

    init

---
+++ electron-app/src/renderer/src/App.tsx
@@ -0,0 +1,10 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+
+function App(): JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return <></>
+}
+
+export default App
